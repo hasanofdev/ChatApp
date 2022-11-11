99aa34026020262ed169ab8314a5b0074f90e0bd
hasanofdev <hesenovelsad468@gmail.com>
2022-11-12 03:29:22 +0400

    Add project files.

---
+++ ChatApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrWhiteSpace(MessageTxt.Text))
+                return;
+
+            MessageControl msgcontrol = new MessageControl(MessageTxt.Text);
+            
+            MessagesPanel.Items.Add(msgcontrol);
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(UsernameTxt.Text))
+                MessageBox.Show("Username Is Incorrect!");
+            UserChip.Content = UsernameTxt.Text.ToUpper();
+            LogInGrid.Visibility = Visibility.Collapsed;
+        }
+    }
+}
